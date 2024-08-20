a784c2392c3607812519658f82b75624387b8b76
MisaITJohan <johan.marmen@misa.se>
2024-08-20 14:38:37 +0200

    Add exercises for W01

---
+++ Exercises/exercises01/W01_exercise02.py
@@ -0,0 +1,9 @@
+# Skriv kod som beräknar summan av alla tal från 1 till och med 10.
+# Summan ska därefter skrivas ut.
+# Siffran som ska skrivas ut är alltså 55.
+
+# Det finns flera möjliga sätt att lösa denna uppgift på, vilket sätt ni
+# löser uppgiften på är valfritt.
+# Att hårdkoda, d.v.s. att skriva print(55) är INTE rätt.
+
+# Ett möjligt sätt att lösa denna uppgift är att använda sig av en while-loop.
---
+++ Exercises/exercises01/W01_exercise04.py
@@ -0,0 +1,13 @@
+# Importera funktionen choice ifrån modulen random och skriv en egen funktion
+# som tar emot en lista med val, väljer ut ett slumpat element och skriver ut
+# det valda elementet.
+
+# Koden nedan är enbart för att kontrollera att övningen är rätt; ändra anropen
+# så att de anropar din funktion.
+
+
+
+
+din_funktion([1, 2, 3, 4])
+din_funktion(["Ett", "Två", "Tre"])
+din_funktion([1, "Två", 3, "Fyra"])
