14f13f27cc67b5df80ec0e4565a8f42c8f3aa8a5
MisaITJohan <johan.marmen@misa.se>
2024-08-26 18:55:00 +0200

    Complete barebones guessing game

--- Lessons/lesson02/hangman/hangman.py
+++ Lessons/lesson02/hangman/hangman.py
@@ -13,47 +13,77 @@ POSSIBLE_WORDS = (
 
 
 class HangmanGame:
+
     def __init__(self, possible_words=None, allowed_guesses=5):
         if possible_words is None:
             self.possible_words = POSSIBLE_WORDS
         self.allowed_guesses = allowed_guesses
+        self.incorrect_guesses_made = 0
         self.word_to_guess = ""
         self.get_word_to_guess()
         self.guessed_letters = set()
+        self.current_guess = ""
+        self.display_current_state()
+
+    def setup(self):
+        """Här borde vissa delar av __init__ ligga för att korta ned den
+        metoden.
+        """
+        pass
 
     def get_word_to_guess(self):
-        self.word_to_guess = random.choice(self.possible_words)
+        self.word_to_guess = random.choice(self.possible_words).lower()
 
+    def display_current_state(self):
+        print("Det hemliga ordet är", len(self.word_to_guess), "tecken långt.")
+        if len(self.guessed_letters) > 0:
+            print("Du har gissat dessa bokstäver:", *self.guessed_letters)
+            print("Du har gissat fel", self.incorrect_guesses_made, "gånger.")
+        print("Du har", self.allowed_guesses - self.incorrect_guesses_made, "gissningar kvar.")
+        self.make_guess()
 
-class GameController:
-    # current_game: HangmanGame
-
-    def __init__(self):
-        self.start_new_game()
-        self.current_guess = ""
-        self.current_game = self.start_new_game()
-
-    def start_new_game(self):
-        return HangmanGame()
-
-    def check_guess(self):
-        if self.current_guess in self.current_game.word_to_guess:
+    def make_guess(self):
+        guess = ""
+        while guess in self.guessed_letters or len(guess) != 1:
+            guess = input("Gissa en bokstav: ").lower()
+        self.guessed_letters.add(guess)
+        self.current_guess = guess
+        check_correct = self.check_guess()
+        if check_correct is True:
             self.correct_guess()
-            return True
         else:
             self.incorrect_guess()
+        self.display_current_state()
+
+
+    def check_guess(self):
+        if self.current_guess in self.word_to_guess:
+            return True
+        else:
             return False
 
     def correct_guess(self):
-        pass
+        print(self.current_guess.upper(), "finns i det hemliga ordet.\n")
+        self.check_game_won()
 
     def incorrect_guess(self):
-        pass
+        print(self.current_guess.upper(), "finns inte i det hemliga ordet.\n")
+        self.incorrect_guesses_made += 1
+        self.check_game_over()
+
+    def check_game_won(self):
+        for letter in self.word_to_guess:
+            if letter not in self.guessed_letters:
+                return
+        print("Du vann! Det hemliga ordet var", self.word_to_guess)
+        quit()
 
 
-def start_hangmangame():
-    game = GameController()
+    def check_game_over(self):
+        if self.incorrect_guesses_made == self.allowed_guesses:
+            print("Game over! Det hemliga ordet var", self.word_to_guess)
+            quit()
 
 
 if __name__ == '__main__':
-    start_hangmangame()
+    game = HangmanGame()
