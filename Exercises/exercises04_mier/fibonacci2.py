76bc550a521fd15e972c02fc6f3e6e54a6570d24
mier <mier@tanglewreck.mier.org>
2024-09-17 09:46:29 +0200

    Simplified. Now takes only one integer, N (defaults to 10), as argument and prints the sequence up until Fibonacci number N

--- Exercises/exercises04_mier/fibonacci2.py
+++ Exercises/exercises04_mier/fibonacci2.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 """
-Fibonacci numbers coded with a generator function
-If executed on the command line, takes an integer 
-argument N and produces a list of fibonacci numbers
-up to the Nth number. If no argument is given,
-prints the first ten fibonacci numbers.
-
 2024-09-13
+Fibonacci numbers coded with a generator function
+
+If executed on the command line, takes an integer N
+as argument and produces the fibonacci sequence
+up until Fibonacci number N.
+
+If no argument is given, N defaults to 10
+
 """
 
 import sys
@@ -15,13 +17,18 @@ import sys
 # sys.set_int_max_str_digits(100000)
 
 try:
-    args = list(map(int, sys.argv[1:]))
+    # args = list(map(int, sys.argv[1:]))
+    N = int(sys.argv[1])
 except ValueError as e:
     print("Non-number argument:", e)
     raise SystemExit(1)
+except IndexError:
+    # args = [10]
+    N = 10
 
-if not args:
-    args = [10]
+#if not args:
+#    args = [10]
+#    N = 10
 
 def fib2(n: int = 10):
     """
@@ -30,6 +37,7 @@ def fib2(n: int = 10):
     a = [1, 1]  # First two fibonacci numbers
     for k in range(2, n + 1):
         a.append(a[k - 1] + a[k - 2])
+    for k in range(n + 1):
         yield a[k]
 
 if __name__ == "__main__":
@@ -38,14 +46,23 @@ if __name__ == "__main__":
     #print(f"Fibonacci number {1:<8d} {1:<10d}")
     #for m, fib in enumerate(fib2(N)):
     #    print(f"Fibonacci number {m + 2:<8d} {fib:<10d}")
-    for k in args:
-        try:
-            F = list(fib2(k))
-            F_n = F[-1]
-            F_n_1 = F[-2]
-            print(F_n, F_n_1)
-            print(F_n - F_n_1)
-        except IndexError:
-            pass
-        except ValueError as e:
-            print(e)
+    # print(args)
+    # for k in args:
+#    for k in range(2, args[0]):
+#        try:
+#            # Generate all Fibonacci numbers
+#            # up to Fib.no. k, which is the
+#            # number we seek.
+#            F = list(fib2(k))
+#            F_n = F[-1]
+#            F_n_1 = F[-2]
+#            # print(F_n, F_n_1)
+#            # print(F[-1])
+#            #print(F_n - F_n_1)
+#        except IndexError:
+#            pass
+#        except ValueError as e:
+#            print(e)
+    F = list(fib2(N))
+    print(f"Fibonacci #{N} = {F[-1]}")
+    print(F)
