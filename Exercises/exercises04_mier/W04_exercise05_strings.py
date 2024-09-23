01bf4424459b52dff832b8f3e5accf17b42a2505
mier <mier@tanglewreck.mier.org>
2024-09-23 13:14:44 +0200

    String exercises

---
+++ Exercises/exercises04_mier/W04_exercise05_strings.py
@@ -0,0 +1,61 @@
+# ÖVNING X
+#
+# Det finns en mängd sträng-metoder som kan användas för att
+# göra saker med strängar, t.ex. 
+#       - göra om en sträng till stora bokstäver: str.upper()
+#       - göra om en sträng till små bokstäver: str.lower():
+#
+# Exempel på hur str.upper() och str.lower() kan användas:
+#
+s = "kanelbullar innehåller kanel"
+print(s.upper())        # Skriver ut "KANELBULLAR INNEHÅLLER KANEL"
+
+S ="KANELBULLAR INNEHÅLLER KANEL"
+print(S.lower())        # Skriver ut "kanelbullar innehåller kanel"
+
+
+#
+# Här är en länk till dokumentationen för sträng-metoder:
+#
+#       https://docs.python.org/3/library/stdtypes.html#string-methods
+#
+#
+# 1. Öppna länken och försök hitta en sträng-metod som gör att
+#
+#       - första bokstaven i strängen blir VERSAL (stor bokstav)
+#       - övriga bokstäver gemener (små bokstäver)
+#
+#    Använd sedan metoden på strängvariablerna s och S nedan:
+#       
+#       s = "kanelbullar innehåller kanel"
+#       S = "KANELBULLAR INNEHÅLLER KANEL"
+#       print(s._ _ _())   # Ersätt "_ _ _" med namnet på metoden.
+#       print(S._ _ _())   # Ersätt "_ _ _" med namnet på metoden.
+#
+#    Vad blir resultatet (vad skrivs ut)?
+#
+#   
+#
+# 2. Leta rätt på en strängmetod som gör att
+#
+#       - första bokstaven i varje ord i strängen görs om till versaler
+#       - övriga bokstäver görs om till gemener
+#
+#       Använd sedan metoden på strängvariablerna i förra uppgiften.
+#
+#    Vad blir resultatet (vad skrivs ut)?
+#
+#
+#
+# 3. Leta rätt på en strängmetod som 
+#
+#       - tar argumenten 'width' och 'fillchar'
+#       - centrerar strängen inom den angivna bredden (width)
+#
+#   Vad blir resultatet av följande kod (ersätt '_ _ _' med namnet på metoden):
+#   
+#       print(s._ _ _(50))
+#       print(s._ _ _(50, '_')
+#
+#
+# 4. Samma som i uppgift 3, men med högerställd text. Vad skrivs ut?
