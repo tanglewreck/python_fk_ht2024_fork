03514692ccae0245d6151cd28be5f6a2e42c1a7b
mier <mier@localhost>
2024-10-02 14:16:14 +0200

    Removed accidentally copied directories/files

--- Exercises/exercises06/exercises04/W04_exercise01.py
+++
@@ -1,3 +0,0 @@
-# Tilldela strängvariabeln "name" något värde (t.ex. "Nisse Hult") och använd sedan
-# en f-sträng för att skriva ut en hälsningsfras (t.ex. "Hej Nisse Hult!") som
-# hämtar namnet i hälsningen från variabeln "name".
